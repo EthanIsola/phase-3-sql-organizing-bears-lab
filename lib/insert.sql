168bba5fec6d235bf762090c52320f5545b800bc
Ethan Isola <isolaethan@gmail.com>
2022-02-25 00:01:56 -0600

    commit tree d0ea9bc0d33bde0a430a1d03b77fa7be0425a838

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'male', 'black', 'aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('A', 1, 'male', 'black', 'aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('B', 2, 'male', 'black', 'aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('C', 3, 'male', 'black', 'aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('D', 4, 'male', 'black', 'aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('E', 5, 'male', 'black', 'aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('F', 6, 'male', 'black', 'aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('G', 7, 'male', 'black', 'aggressive', true);
